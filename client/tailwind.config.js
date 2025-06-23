0b6a02dcaeb6ec2af89ae07efaee10cdfdc71fc4
Djembaraa <djembararafat98@gmail.com>
2025-06-24 01:29:59 +0700

    feat: configure tailwindcss v4 with vite plugin

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
+// Impor plugin baru yang sudah kita install
+import tailwindcss from "@tailwindcss/vite";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // Daftarkan plugin di sini
+  plugins: [tailwindcss()],
 };
